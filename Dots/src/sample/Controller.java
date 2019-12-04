35904516f0e0dcaec7958a198503eac6805aa37f
Adel Khadiev <rittenzore@gmail.com>
2019-12-04 15:22:12 +0300

    push

---
+++ Dots/src/sample/Controller.java
@@ -0,0 +1,54 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button singlePlayerButton;
+
+    @FXML
+    private Button multiPlayerButton;
+
+    @FXML
+    private Button playWithAiButton;
+
+    @FXML
+    void initialize() {
+        singlePlayerButton.setOnAction(actionEvent -> {
+            openNewScene("/sample/singleplayer.fxml");
+        });
+    };
+
+    public void openNewScene(String window) {
+        singlePlayerButton.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(window));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
+}
